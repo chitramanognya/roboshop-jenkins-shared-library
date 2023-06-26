0503f7ccbd47414bbdd1baa60e58d3dad6a33a0e
chitra <chmanu.chinu@gmail.com>
2023-06-26 12:47:22 +0000

    update library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,12 +6,7 @@ pipeline {
         stage('Compile/Build') {
             steps {
                 script {
-                if(app_lang == "nodejs") {
-                    sh 'npm install'
-                }
-                if(app_lang == "maven") {
-                    sh 'mvn package'
-                }
+                common.compile()
             }
         }
         
