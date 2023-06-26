8411cb498556f72db4e73c202e7cb67f6e3ae8f4
chitra <chmanu.chinu@gmail.com>
2023-06-26 06:20:45 +0000

    update library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,18 +2,27 @@ def call() {
     pipeline {
       agent any    
       
+      parameters {
+          string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+      }
+      
+      options {
+          ansicolor('xterm')
+      }
       stages {
           
           stage('init') {
               steps {
                   
-                  sh 'terraform init -backend-config=env-dev/state.tfvars'
+                  sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
               }
           }
           
           stage('apply') {
               steps {
-                  sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                 // sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+              
+                  sh 'echo'
               }
           }
       }  
