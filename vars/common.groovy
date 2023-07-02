94c709eed4128f2bd67a506d055aebf478d9822b
chitra <chmanu.chinu@gmail.com>
2023-07-02 05:49:55 +0000

    update library

--- vars/common.groovy
+++ vars/common.groovy
@@ -17,5 +17,5 @@ def testcases() {
 }
 def codequality() {
    
- sh 'sonar-scanner -Dsonar.host.url=http://172.31.84.159:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'   
+ sh 'sonar-scanner -Dsonar.host.url=http://172.31.84.159:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component}'   
 }
